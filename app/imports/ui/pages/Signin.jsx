1962440f1a2a20720b2717164dcfa70aed3b0cef
Yeji Han <hany7@hawaii.edu>
2021-10-21 15:15:04 -1000

    changed path the page after singin

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -38,7 +38,7 @@ export default class Signin extends React.Component {
     const { from } = this.props.location.state || { from: { pathname: '/' } };
     // if correct authentication, redirect to page instead of login screen
     if (this.state.redirectToReferer) {
-      return <Redirect to={from}/>;
+      return <Redirect to={{ pathname: '' }}/>;
     }
     // Otherwise return the Login form.
     return (

eadf436126c003cf050d9d57a96c0429c96b7a7b
Yeji Han <hany7@hawaii.edu>
2021-10-21 14:55:55 -1000

    changed signout page

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Header } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
@@ -8,7 +9,14 @@ export default class Signout extends React.Component {
     Meteor.logout();
     return (
       <Header id="signout-page" as="h2" textAlign="center">
-        <p>You are signed out.</p>
+        <br />
+        <p className="signout-page-2" style={{ color: "rgb(44, 62, 80)" }}>You have been logged out.</p>
+        <div>
+          <img className="signout-image" src="images/mask4.png"/></div>
+        <br/>
+        <Link to="/">
+          <Button size="big" style={{ color: "rgb(44, 62, 80)" }} to="/">Return to Home</Button>
+        </Link>
       </Header>
     );
   }

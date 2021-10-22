2b0a5bb78071064f41def6807455a30f0bdc7f81
Yeji Han <hany7@hawaii.edu>
2021-10-21 16:12:12 -1000

    added "add event" on My Event page

--- app/imports/ui/pages/ListEvent.jsx
+++ app/imports/ui/pages/ListEvent.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Card, Container, Header, Loader } from 'semantic-ui-react';
+import { Button, Card, Container, Grid, Header, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Events } from '../../api/event/Event';
 import EventItem from '../components/EventItem';
+import { Link } from 'react-router-dom';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListEvent extends React.Component {
@@ -22,6 +23,14 @@ class ListEvent extends React.Component {
           <Header as="h1" style={{ color: "rgb(44, 62, 80)" }} textAlign="center">My Events</Header>
           <br />
           <br />
+          <h3>
+            <p style={{ color: "rgb(44, 62, 80)" }}>
+              Add your event now!
+            </p>
+          </h3>
+          <Link to="/add">
+            <Button size="large" style={{ color: "rgb(44, 62, 80)" }} to="/add">Add Event</Button>
+          </Link>
           <Card.Group>
             {this.props.events.map((event) => <EventItem key={event._id} event={event} Events={Events} />)}
           </Card.Group>
